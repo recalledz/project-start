18d78752b09051a843914fb10f743b36dbea044d
recalledz <f.froilanrivera@gmail.com>
2025-07-16 08:01:47 -0400

    Extend JS globals and add mocha test config

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,25 @@ import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
+const jsGlobals = {
+  ...globals.browser,
+  ...globals.node,
+  PIXI: "readonly",
+  lucide: "readonly",
+};
+
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: jsGlobals },
+  },
+  {
+    files: ["test/**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: { ...jsGlobals, ...globals.mocha } },
+  },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
   { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
