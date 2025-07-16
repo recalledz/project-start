56bca9cf88e6f1d20e84588389e48940ac4b2e80
recalledz <f.froilanrivera@gmail.com>
2025-07-16 15:45:17 -0400

    Remove unused attribute point helpers

--- attributes.js
+++ attributes.js
@@ -34,17 +34,9 @@ export const attributes = {
   }
 };
 
-export function addStrength(points = 1) {
-  attributes.Strength.points += points;
-}
-
 export function strengthXpMultiplier(task) {
   const affected = ['Log Pine', 'Mining', 'Smithing'];
   return affected.includes(task) ? 1 + attributes.Strength.points * 0.1 : 1;
-}
-
-export function addEndurance(points = 1) {
-  attributes.Endurance.points += points;
 }
 
 export function enduranceXpMultiplier(task) {
@@ -52,17 +44,9 @@ export function enduranceXpMultiplier(task) {
   return affected.includes(task) ? 1 + attributes.Endurance.points * 0.1 : 1;
 }
 
-export function addDexterity(points = 1) {
-  attributes.Dexterity.points += points;
-}
-
 export function dexterityXpMultiplier(task) {
   const affected = ['Woodcutting', 'Gather Fruit'];
   return affected.includes(task) ? 1 + attributes.Dexterity.points * 0.1 : 1;
-}
-
-export function addIntelligence(points = 1) {
-  attributes.Intelligence.points += points;
 }
 
 export function intelligenceXpMultiplier(task) {
