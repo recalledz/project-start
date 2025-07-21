78bf7407b69f24f4fdbb73c520539d4268e78fdf
recalledz <f.froilanrivera@gmail.com>
2025-07-21 10:05:38 -0400

    Update sectInit.js

--- utils/sectInit.js
+++ utils/sectInit.js
@@ -1,11 +1,14 @@
 import Disciple from '../disciple.js';
 import { initializeDisciple } from './discipleInit.js';
+import { generateSkillAffinities, initializeStartingSkills } from '../game/affinities.js';
 import { sectState } from '../game/state.js';
 
 export function initializeSect() {
   const disciples = [1, 2, 3].map(id => {
     const d = new Disciple({ id });
     initializeDisciple(d);
+    d.affinities = generateSkillAffinities();
+    initializeStartingSkills(d);
     return d;
   });
   sectState.fruits = 100;
